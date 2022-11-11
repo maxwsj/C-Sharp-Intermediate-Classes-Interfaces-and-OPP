2c2e80bdc00e9a80b697cf105b0bbe86023ccc71
maxwsj <max.ntk@hotmail.com>
2022-11-10 22:30:06 -0300

    setup inicial

---
+++ Section2-Classes/PersonClass/PersonClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PersonClass
+{
+    class Program
+    {
+        public class Person
+        {
+            public string Name;
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine($"Hi {to}, I am {Name}");
+            }
+
+            // We are creating a personal object from a string
+            // We create a parse method
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+
+                return person;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Max");
+            person.Introduce("Andressa");
+        }
+    }
+}
---
+++ Section2-Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PersonClass
+{
+    class Program
+    {
+        public class Person
+        {
+            public string Name;
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine($"Hi {to}, I am {Name}");
+            }
+
+            // We are creating a personal object from a string
+                // We create a parse method
+            public static Person Parse(string str)
+            {
+                var person = new Person();
+                person.Name = str;
+
+                return person;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
